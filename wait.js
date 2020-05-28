a93c8d7f59ea638ac8e41f63ae788f5a0f9b5640
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2020-05-28 14:42:12 -0300

    commit message

--- wait.js
+++ wait.js
@@ -1,11 +1,2 @@
-let wait = function(milliseconds) {
-  return new Promise((resolve, reject) => {
-    if (typeof(milliseconds) !== 'number') { 
-      throw new Error('milleseconds not a number'); 
-    }
-
-    setTimeout(() => resolve("done!"), milliseconds)
-  });
-}
-
-module.exports = wait;
+blablabla
+kkk
