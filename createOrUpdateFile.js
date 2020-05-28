3237842cb6584a3f8a367e97e3415a0dc984d015
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-05-28 11:46:20 -0300

    implementa createOrUpdateFile

---
+++ createOrUpdateFile.js
@@ -0,0 +1,38 @@
+const createOrUpdateFile = async (options) => {
+  const {
+    client,
+    owner,
+    repo,
+    ref,
+    file,
+  } = options;
+
+  const defaultParams = {
+    owner,
+    repo,
+    path: file.path,
+    message: 'commit message',
+    content: file.content,
+    branch: ref,
+  };
+
+  const params =
+    await client.repos.getContents({
+      owner,
+      repo,
+      path: file.path,
+      ref,
+    }).then(({ data }) => {
+      return Promise.resolve({
+        ...defaultParams,
+        sha: data.sha,
+      });
+    }).catch(() => {
+      return Promise.resolve(defaultParams);
+    });
+
+  const { data: content } = await client.repos.createOrUpdateFile(params);
+  return content;
+};
+
+module.exports = createOrUpdateFile;
---
+++ createOrUpdateFile.test.js
@@ -0,0 +1,100 @@
+const createOrUpdateFile = require('./createOrUpdateFile');
+
+const client = {
+  repos: {
+    getContents: jest.fn(),
+    createOrUpdateFile: jest.fn()
+  }
+};
+
+const run = () => {
+  return createOrUpdateFile({
+    client,
+    owner: 'my-org',
+    repo: 'my-repo',
+    ref: 'heads/my-branch',
+    file: {
+      path: 'wait.js',
+       content: 'YmxhYmxhYmxhCg==',
+    }
+  });
+};
+
+describe('createOrUpdateFile', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('create file', async () => {
+    client.repos.getContents
+      .mockImplementation(() => {
+        return Promise.reject(Error('Not Found'));
+      }
+    );
+    client.repos.createOrUpdateFile.mockResolvedValue({
+      data: {
+        content: {
+          name: 'wait.js',
+          path: 'wait.js',
+          sha: 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
+        }
+      }
+    });
+
+    const file = await run();
+    expect(client.repos.getContents).toHaveBeenCalledWith({
+      owner: 'my-org',
+      repo: 'my-repo',
+      ref: 'heads/my-branch',
+      path: 'wait.js',
+    });
+    expect(client.repos.createOrUpdateFile).toHaveBeenCalledWith({
+      owner: 'my-org',
+      repo: 'my-repo',
+      path: 'wait.js',
+      message: 'commit message',
+      content: 'YmxhYmxhYmxhCg==',
+      branch: 'heads/my-branch',
+    });
+    expect(file.content.name).toBe('wait.js');
+    expect(file.content.path).toBe('wait.js');
+  });
+
+  it('update file', async () => {
+    client.repos.getContents.mockResolvedValue({
+      data: {
+        name: 'wait.js',
+        path: 'wait.js',
+        sha: '88bf17f4a6419d6f0c3b44bfa70c3e572ab938af'
+      }
+    });
+    client.repos.createOrUpdateFile.mockResolvedValue({
+      data: {
+        content: {
+          name: 'wait.js',
+          path: 'wait.js',
+          sha: 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391'
+        }
+      }
+    });
+
+  const file = await run();
+  expect(client.repos.getContents).toHaveBeenCalledWith({
+    owner: 'my-org',
+    repo: 'my-repo',
+    ref: 'heads/my-branch',
+    path: 'wait.js',
+  });
+  expect(client.repos.createOrUpdateFile).toHaveBeenCalledWith({
+    owner: 'my-org',
+    repo: 'my-repo',
+    path: 'wait.js',
+    message: 'commit message',
+    content: 'YmxhYmxhYmxhCg==',
+    branch: 'heads/my-branch',
+    sha: '88bf17f4a6419d6f0c3b44bfa70c3e572ab938af',
+  });
+  expect(file.content.name).toBe('wait.js');
+  expect(file.content.path).toBe('wait.js');
+  });
+});
